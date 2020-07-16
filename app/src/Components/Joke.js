896603251574ac986b30962b49d650a9b6124937
ViridityMoon <viriditymoon@gmail.com>
2020-07-16 12:14:55 -0400

    added some basic styles

--- app/src/Components/Joke.js
+++ app/src/Components/Joke.js
@@ -21,17 +21,18 @@ const Joke = props => {
     return (
         <div className="Joke">
 
-            {props.isLoading && <h4>Loading mission data...</h4>}
+            {props.isLoading && <h4 className='loading'>Loading joke data...</h4>}
 
             {props.error && (
             <p className="error">Error Message: {props.error}</p>
-            )};
+            )}
 
-            <h1>Click the button for a random joke!</h1>
+            <h1 >Click the button for a random joke!</h1>
             <h2>{props.data.setup}</h2>
 
-            <button onClick={onClickFeature}></button>
+            <button className='joke-button' onClick={onClickFeature}>Click Here for the Punchline!</button>
             {props.active ? <Punchline/> : null}
+            <button className='joke-button' onClick={ () => window.location.reload(false)}>Click Here for a New Joke!</button>
             
         </div>
     );
